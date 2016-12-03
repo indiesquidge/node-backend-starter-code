98698c1e7389cb6380082df67a3a58fc1ea84c82
Austin Wood <i@austinwood.me>
2016-12-03 15:24:52 -0800

    Fix errors to get server up and running

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var path = require('path');
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require('body-parser')
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')))
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
-;
+app.get('/favorites', (req, res) => {
+  const data = fs.readFileSync('./data.json')
+  res.setHeader('Content-Type', 'application/json')
+  res.send(data)
+})
 
-app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
-    res.send("Error");
+app.get('favorites', (req, res) => {
+  if (!req.body.name || !req.body.oid) {
+    res.send('Error')
     return
-  
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  }
+
+  const data = JSON.parse(fs.readFileSync('./data.json'))
+  data.push(req.body)
+  fs.writeFile('./data.json', JSON.stringify(data))
+  res.setHeader('Content-Type', 'application/json')
+  res.send(data)
 });
 
-app.list(3000, function(){
-  console.log("Listening on port 3000");
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
 });
