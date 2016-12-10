51f3557a91f2ccf6a8329c1e225d25898cce1cf0
Austin Wood <i@austinwood.me>
2016-12-09 19:04:38 -0800

    Set up proxy between webpack-dev-server and express (workflow)

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
+const httpProxy = require('http-proxy')
 
+const proxy = httpProxy.createProxyServer()
 const app = express()
 
 const isProduction = process.env.NODE_ENV === 'production'
@@ -10,7 +12,31 @@ const publicPath = path.resolve(__dirname, 'public')
 // We point to our static assets
 app.use(express.static(publicPath))
 
-// And run the server
+// We only want to run the workflow when not in production
+if (!isProduction) {
+
+  // We require the bundler inside the if block because
+  // it is only needed in a development environment.
+  const bundle = require('./server/bundle.js')
+  bundle()
+
+  // Any requests to localhost:3000/build
+  // is proxied to webpack-dev-server
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    })
+  })
+
+}
+
+// It is important to catch any errors from the proxy or the
+// server will crash. An example of this is connecting to the
+// server when webpack is bundling
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...')
+})
+
 app.listen(port, function () {
   console.log('Server running on port ' + port)
 })
