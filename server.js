7d6900282d979b6c0b7c0a5b6b115da6b87f24e4
Austin Wood <i@austinwood.me>
2016-12-09 23:13:15 -0800

    Use dev build for production

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const bodyParser = require('body-parser')
 const proxy = httpProxy.createProxyServer()
 const app = express()
 
-const isDeveloping = process.env.NODE_ENV !== 'production'
-const port = isDeveloping ? 3000 : process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use('/assets', express.static(path.join(__dirname, 'app', 'assets')))
 
@@ -49,23 +48,18 @@ app.post('/favorites.json', (req, res) => {
   res.send(favorites)
 });
 
-// We only want to run the workflow when not in production
-if (isDeveloping) {
+// We require the bundler inside the if block because
+// it is only needed in a development environment.
+const bundle = require('./server/bundle.js')
+bundle()
 
-  // We require the bundler inside the if block because
-  // it is only needed in a development environment.
-  const bundle = require('./server/bundle.js')
-  bundle()
-
-  // Any requests to localhost:3000/build
-  // is proxied to webpack-dev-server
-  app.all('/build/*', (req, res) => {
-    proxy.web(req, res, {
-      target: 'http://localhost:8080'
-    })
+// Any requests to localhost:3000/build
+// is proxied to webpack-dev-server
+app.all('/build/*', (req, res) => {
+  proxy.web(req, res, {
+    target: 'http://localhost:8080'
   })
-
-}
+})
 
 // It is important to catch any errors from the proxy or the
 // server will crash. An example of this is connecting to the
@@ -75,5 +69,5 @@ proxy.on('error', () => {
 })
 
 app.listen(port, () => {
-  console.log('Server running on port ' + port)
+  console.log(`Server running on port ${port}`)
 })
