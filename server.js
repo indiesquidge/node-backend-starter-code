6bcf46bbda18cc3caae85f2f1467aafc6f7dfa2e
Austin Wood <i@austinwood.me>
2016-12-09 19:24:06 -0800

    Basic Express routing for static files

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer()
 const app = express()
 
-const isProduction = process.env.NODE_ENV === 'production'
-const port = isProduction ? process.env.PORT : 3000
-const publicPath = path.resolve(__dirname, 'public')
+const isDeveloping = process.env.NODE_ENV !== 'production'
+const port = isDeveloping ? 3000 : process.env.PORT
 
-// We point to our static assets
-app.use(express.static(publicPath))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.get('/favorites', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/favorites.html'))
+})
 
 // We only want to run the workflow when not in production
-if (!isProduction) {
+if (isDeveloping) {
 
   // We require the bundler inside the if block because
   // it is only needed in a development environment.
@@ -22,9 +26,9 @@ if (!isProduction) {
 
   // Any requests to localhost:3000/build
   // is proxied to webpack-dev-server
-  app.all('/build/*', function (req, res) {
+  app.all('/build/*', (req, res) => {
     proxy.web(req, res, {
-        target: 'http://localhost:8080'
+      target: 'http://localhost:8080'
     })
   })
 
@@ -33,10 +37,10 @@ if (!isProduction) {
 // It is important to catch any errors from the proxy or the
 // server will crash. An example of this is connecting to the
 // server when webpack is bundling
-proxy.on('error', function(e) {
+proxy.on('error', () => {
   console.log('Could not connect to proxy, please try again...')
 })
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log('Server running on port ' + port)
 })
